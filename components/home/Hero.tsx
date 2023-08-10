101bede4f7b3aff9f09aa1907103762c2d8f3ba6
Sahil Satpute <explore.sahil1@gmail.com>
2023-08-10 20:53:08 +0530

    updated hero

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -2,6 +2,7 @@
 
 import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import Star from "../icons/Star";
 import Img from "../default/Img";
@@ -9,9 +10,13 @@ import fourMen from "@/public/images/assets/four-men.png";
 import SplitType from "split-type";
 
 const Hero = () => {
+  const scrollInfoRef = useRef(null);
+
   useLayoutEffect(() => {
     const textOne = new SplitType(".textOne", { types: "lines" });
     const textTwo = new SplitType(".textTwo", { types: "lines" });
+
+    gsap.registerPlugin(ScrollTrigger);
 
     gsap.fromTo(
       textOne.lines,
@@ -42,16 +47,35 @@ const Hero = () => {
         delay: 3,
       }
     );
+
+    gsap.fromTo(
+      scrollInfoRef.current,
+      {
+        opacity: 1,
+        y: 0,
+      },
+      {
+        opacity: 0,
+        y: -50,
+        scrollTrigger: {
+          trigger: scrollInfoRef.current,
+          //markers: true,
+          scrub: 0.2,
+          start: "100px bottom",
+          end: "100px 600px",
+        },
+      }
+    );
   }, []);
 
   return (
     <section className="hero-container">
       <div className="container">
         <div className="headline">
-          <p className="textOne" data-scroll data-scroll-speed="0.8">
+          <p className="textOne" data-scroll data-scroll-speed="0.6">
             I Craft Digital Experiences,
           </p>
-          <div className="line-second" data-scroll data-scroll-speed="0.8">
+          <div className="line-second" data-scroll data-scroll-speed="0.6">
             <p className="textTwo">
               Turning <span className="pink">pixels</span> into{" "}
               <span className="pink">possibilities</span>
@@ -67,6 +91,10 @@ const Hero = () => {
             alt="four men standing nextb to each other poster"
           />
         </div>
+      </div>
+      <div className="scroll-info" ref={scrollInfoRef}>
+        <p>portfolio</p>
+        <p>scroll baby scroll</p>
       </div>
     </section>
   );
