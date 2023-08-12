6427fb6d7af33e22584162757896c5f2be92d501
Sahil Satpute <explore.sahil1@gmail.com>
2023-08-12 17:22:49 +0530

    portfolio updated

--- components/home/Portfolio.tsx
+++ components/home/Portfolio.tsx
@@ -1,7 +1,13 @@
+import LoadingAnim from "../default/LoadingAnim";
+
 const Portfolio = () => {
   return (
     <div className="portfolio-container" id="portfolio">
-      Portfolio
+      <div className="container">
+        <h3>Portfolio</h3>
+        <p>commin soon</p>
+        <LoadingAnim />
+      </div>
     </div>
   );
 };
