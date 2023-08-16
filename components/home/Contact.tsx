ea13a36cd41d44874cf80b771dbb3a88b67805ff
Sahil Satpute <explore.sahil1@gmail.com>
2023-08-16 18:30:48 +0530

    updated contact

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -11,7 +11,6 @@ import clouds from "@/public/images/assets/pink-clouds.png";
 import Img from "../default/Img";
 import Star from "../icons/Star";
 import LoadingAnim from "../default/LoadingAnim";
-import GsapMagnetic from "../default/GsapMagnetic";
 
 const Crystal = dynamic(() => import("../crystal/Crystal"), {
   ssr: false,
@@ -122,24 +121,22 @@ const Contact = () => {
                 required
               ></textarea>
             </div>
-            <GsapMagnetic>
-              <div className="button-container">
-                <button
-                  type="submit"
-                  className={isLoading ? "loading-button" : ""}
-                >
-                  {isLoading ? (
-                    <LoadingAnim />
-                  ) : isEmailSent ? (
-                    <>
-                      Sent! <TiTick />
-                    </>
-                  ) : (
-                    "Send"
-                  )}
-                </button>
-              </div>
-            </GsapMagnetic>
+            <div className="button-container">
+              <button
+                type="submit"
+                className={isLoading ? "loading-button" : ""}
+              >
+                {isLoading ? (
+                  <LoadingAnim />
+                ) : isEmailSent ? (
+                  <>
+                    Sent! <TiTick />
+                  </>
+                ) : (
+                  "Send"
+                )}
+              </button>
+            </div>
           </form>
           <MouseParallax
             isAbsolutelyPositioned
