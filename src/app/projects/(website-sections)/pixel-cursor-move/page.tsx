a78b96a8639711af86d70849a242375fd375a426
Sahil Satpute <explore.sahil1@gmail.com>
2023-11-11 13:37:38 +0530

    deleted file

--- src/app/projects/(website-sections)/pixel-cursor-move/page.tsx
+++
@@ -1,7 +0,0 @@
-import HoverImageSlideEffect from "@/components/projects/hover-image-slide-effect/HoverImageSlideEffect";
-
-const page = () => {
-  return <HoverImageSlideEffect />;
-};
-
-export default page;
